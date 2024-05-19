e8ca488930d2319adbe934c1110f3399174a13e0
Lee gangho <rkdgh1198@gmail.com>
2024-05-19 18:59:01 +0900

    Updates

--- admin.js
+++ admin.js
@@ -1,31 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const generateUniqueId = () => {
-    let currentIndex = localStorage.getItem("currentIndex");
-    if (!currentIndex) {
-      currentIndex = 1;
-    } else {
-      currentIndex = parseInt(currentIndex, 10) + 1;
-    }
-    localStorage.setItem("currentIndex", currentIndex);
-    return currentIndex;
-  };
-
   const loadBoxesFromLocalStorage = () => {
     const keys = Object.keys(localStorage).filter((key) =>
       key.startsWith("box-")
     );
     keys.forEach((key) => {
-      const { id, boxContent, className } = JSON.parse(
-        localStorage.getItem(key)
-      );
-      addBoxToBotDiv(id, boxContent, className, false);
+      const { id, inputValue } = JSON.parse(localStorage.getItem(key));
+      addBoxToBotDiv(id, inputValue, "new-box", false);
     });
-  };
-
-  const saveBoxToLocalStorage = (id, boxContent, className) => {
-    const key = `box-${id}`;
-    const boxData = { id, boxContent, className };
-    localStorage.setItem(key, JSON.stringify(boxData));
   };
 
   const addBoxToBotDiv = (
@@ -34,8 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
     className,
     saveToLocalStorage = true
   ) => {
-    console.log("saveToLocalStorage:", saveToLocalStorage);
-
     const botDiv = document.querySelector(".Bot");
     if (botDiv) {
       const newBox = document.createElement("div");
@@ -46,24 +25,13 @@ document.addEventListener("DOMContentLoaded", () => {
       newBox.id = id;
       botDiv.appendChild(newBox);
       if (saveToLocalStorage) {
-        saveBoxToLocalStorage(id, boxContent, className);
+        localStorage.setItem(
+          id,
+          JSON.stringify({ id, inputValue: boxContent, className })
+        );
       }
     }
   };
-
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://chickengir1.github.io/LLMfront/",
-  ];
-
-  window.addEventListener("message", (event) => {
-    if (allowedOrigins.includes(event.origin)) {
-      const { id, boxContent, className } = event.data;
-      if (boxContent) {
-        addBoxToBotDiv(id, boxContent, className);
-      }
-    }
-  });
 
   loadBoxesFromLocalStorage();
 });
